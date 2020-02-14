6d0a7436e60377233c930e69e50932092718afc5
gicchan12 <gicchan812@gmail.com>
2020-02-14 14:18:27 +0900

    testですよん

---
+++ AddCity/Test1.swift
@@ -0,0 +1,21 @@
+//
+//  Test1.swift
+//  AddCity
+//
+//  Created by Masahiro on 2020/02/14.
+//  Copyright © 2020 AddCity. All rights reserved.
+//
+
+import SwiftUI
+
+struct Test1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Test1_Previews: PreviewProvider {
+    static var previews: some View {
+        Test1()
+    }
+}
